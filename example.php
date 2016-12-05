17bd286e9a81a621c9b5f12a7368b5120cec1aec
Tim Roberts <timiroberts@gmail.com>
2016-12-04 22:58:11 -0500

    Added an example usage case for API
    This can be placed anywhere as long as it gets ran after WordPress reads the Bearded Router and Bearded Route classes

---
+++ example.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+  * This is inside of your own plugin
+  * that you want to create endpoints
+  * to
+  */
+  
+function plugin_handle_users_get($request,$route){
+  $users = []; // got users based on request
+  return $route->send($users);
+}
+
+function plugin_handle_user_post($request,$route){
+  $user_created = true; // handle create user request
+  return $route->send($user_created);
+}
+
+function plugin_handle_get_user($request,$route){
+  $id = $request->get_param('id');
+  $user = []; // got user from db
+  return $route->send($user);
+}
+
+function plugin_add_routes(){
+  $router = new BeardedRouter('/plugin-api');
+  $router->add_route('/users','plugin_handle_users_get');
+  $router->add_route('/users','['post'=>'plugin_handle_user_post');
+  $router->add_route('/users/:id',['get'=>'plugin_handle_get_user']);
+}
+
+add_action('rest_api_init','plugin_add_routes');
